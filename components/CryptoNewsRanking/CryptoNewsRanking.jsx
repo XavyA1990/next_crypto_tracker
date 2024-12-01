efc5a43eaa551984b9eb9d5d15cf9ed3fc419082
XavyA1990 <contrerasdeveloper@outlook.com>
2024-12-01 14:40:19 -0500

    Add loading state to CryptoNewsRanking component and handle fetch errors

--- components/CryptoNewsRanking/CryptoNewsRanking.jsx
+++ components/CryptoNewsRanking/CryptoNewsRanking.jsx
@@ -7,18 +7,34 @@ import Text from "../Text/Text";
 
 const CryptoNewsRanking = () => {
   const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchCryptoNewsRanking().then((data) => {
-      setNews(data.data);
-    });
+    fetchCryptoNewsRanking()
+      .then((data) => {
+        setNews(data.data);
+      })
+      .catch((error) => {
+        console.error("🚀 ~ useEffect ~ error", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
   return (
     <div className="mt-16 relative isolate">
-      <Text variant={"h3"} customClasses={"px-3 mb-8"}>
-        Noticias de Criptomonedas con mejor ranking
-      </Text>
-      <NewsCardLayout data={news} />
+      {!loading && (
+        <>
+          <Text
+            variant={"h3"}
+            colorType={"text-title"}
+            customClasses={"px-3 mb-8"}
+          >
+            Noticias de Criptomonedas con mejor ranking
+          </Text>
+          <NewsCardLayout data={news} />
+        </>
+      )}
     </div>
   );
 };
