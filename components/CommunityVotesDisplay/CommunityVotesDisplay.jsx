161fba8368e2b7f7c2cc6f6f13aa050ae59479ce
XavyA1990 <contrerasdeveloper@outlook.com>
2024-12-06 11:49:58 -0500

    Refactor cryptocurrency display components; consolidate logic into CryptocurrencyShow and update CommunityVotesDisplay with localized labels

--- components/CommunityVotesDisplay/CommunityVotesDisplay.jsx
+++ components/CommunityVotesDisplay/CommunityVotesDisplay.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Text from "../Text/Text";
+import labels from "@/lib/labels/labels.json";
 
-const CommunityVotesDisplay = ({bullish, bearish, name}) => {
+const { title, bullishLabel, bearishLabel, voteStatement } = labels.communityVotes;
+
+const CommunityVotesDisplay = ({ bullish, bearish, name }) => {
   return (
     <div className="w-full flex mt-4 gap-4 flex-col">
       <Text variant={"h3"} colorType={"normal-text"}>
-        Votos de la comunidad
+        {title}
       </Text>
       <div className="w-full bg-gray-200 rounded-full h-6 relative flex overflow-hidden">
         <div
@@ -21,19 +24,12 @@ const CommunityVotesDisplay = ({bullish, bearish, name}) => {
           }}
         ></div>
         <div className="absolute inset-0 flex items-center justify-center font-bold">
-          {bullish > bearish
-            ? `${bullish}%`
-            : `${bearish}%`}
+          {bullish > bearish ? `${bullish}%` : `${bearish}%`}
         </div>
       </div>
       <Text colorType={"normal-text"}>
-        De acuerdo al{" "}
-        {bullish > bearish
-          ? bullish
-          : bearish}
-        % de los votantes,{" "}
-        <span className="font-semibold uppercase">{name}</span> tiene
-        una tendencia{" "}
+        {`${voteStatement[0]} ${bullish > bearish ? bullish : bearish}${voteStatement[1]} `}
+        <span className="font-semibold uppercase">{name}</span> {voteStatement[2]}{" "}
         <span
           className={
             bullish > bearish
@@ -42,8 +38,8 @@ const CommunityVotesDisplay = ({bullish, bearish, name}) => {
           }
         >
           {bullish > bearish
-            ? "ALCISTA"
-            : "BAJISTA"}
+            ? bullishLabel.toUpperCase()
+            : bearishLabel.toUpperCase()}
         </span>
         .
       </Text>
--- components/CryptocurrencyShow/CryptocurrencyShow.jsx
+++ components/CryptocurrencyShow/CryptocurrencyShow.jsx
@@ -1,11 +1,124 @@
-import React from 'react'
+"use client";
+
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import PageTitle from "../PageTitle/PageTitle";
+import Text from "../Text/Text";
+import FavoritesButton from "../FaviritesButton/FavoritesButton";
+import CommunityVotesButtons from "../CommunityVotesButtons/CommunityVotesButtons";
+import Link from "../Link/Link";
+import CommunityVotesDisplay from "../CommunityVotesDisplay/CommunityVotesDisplay";
+import CandleStickChart from "../CandleStickChart/CandleStickChart";
+import CryptoMonthlyTable from "../CryptoMonthlyTable/CryptoMonthlyTable";
+import {
+  fetchCryptocurrencyInfo,
+  fetchUserCryptoPreference,
+} from "@/services/crypto";
+import Spinner from "../Spinner/Spinner";
+import { usePathname } from "next/navigation";
+import { useAuthStore } from "@/store/globalStore";
+import labels from "@/lib/labels/labels";
+
+const { seeMore } = labels.commons
 
 const CryptocurrencyShow = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [cryptoInfo, setCryptoInfo] = useState({});
+  const [loading, setLoading] = useState(true);
+  const user = useAuthStore((state) => state.user);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [userVote, setUserVote] = useState("");
+  const [voteStats, setVoteStats] = useState({});
+  const [showVotes, setShowVotes] = useState(false);
+  const path = usePathname();
+  const slug = path.split("/")[2];
 
-export default CryptocurrencyShow
+  useEffect(() => {
+    fetchCryptocurrencyInfo(slug)
+      .then((data) => {
+        setCryptoInfo(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching crypto info", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (user && user.id) {
+      fetchUserCryptoPreference(user.id, slug).then((res) => {
+        const { data } = res;
+        if (data && user?.id) {
+          setIsFavorite(data?.is_favorite);
+          setVoteStats(data?.totals);
+          setUserVote(data?.vote);
+        }
+      });
+    }
+  }, [user, slug]);
+
+  if (loading) {
+    return (
+      <div className="flex items-center h-full justify-center absolute left-1/2 bottom-1/4">
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <PageTitle title={`${cryptoInfo.name}`} imgSrc={cryptoInfo.logo} />
+      <div className="flex flex-col gap-4">
+        <Text variant={"h2"} colorType={"normal-text"}>
+          {cryptoInfo.symbol}
+        </Text>
+        <Text colorType={"normal-text"}>{cryptoInfo.description}</Text>
+        {cryptoInfo.website && (
+          <div
+            className={`flex ${user?.id ? "justify-between" : "justify-end"}`}
+          >
+            {user && user?.id && (
+              <div className="flex gap-4">
+                <FavoritesButton
+                  symbol={cryptoInfo.symbol}
+                  name={cryptoInfo.name}
+                  slug={cryptoInfo.slug}
+                  isFavorite={isFavorite}
+                  setIsFavorite={setIsFavorite}
+                  userId={user.id}
+                />
+                <CommunityVotesButtons
+                  name={cryptoInfo.name}
+                  slug={cryptoInfo.slug}
+                  symbol={cryptoInfo.symbol}
+                  userId={user.id}
+                  userVote={userVote}
+                  setUserVote={setUserVote}
+                  voteStats={voteStats}
+                  setVoteStats={setVoteStats}
+                  showVotes={showVotes}
+                  setShowVotes={setShowVotes}
+                />
+              </div>
+            )}
+            <Link target="_blank" href={cryptoInfo.website} variant={"primary"}>
+              {seeMore}
+            </Link>
+          </div>
+        )}
+      </div>
+      {showVotes && user?.id && (
+        <CommunityVotesDisplay
+          bullish={voteStats.percentages?.bullish}
+          bearish={voteStats.percentages?.bearish}
+          name={cryptoInfo.name}
+        />
+      )}
+      <CandleStickChart symbol={cryptoInfo.symbol} />
+      <CryptoMonthlyTable name={cryptoInfo.name} symbol={cryptoInfo.symbol} />
+    </>
+  );
+};
+
+export default CryptocurrencyShow;
