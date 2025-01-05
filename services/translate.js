33b632e63d04f2cd3bf6c5ae828522222d041e03
XavyA1990 <contrerasdeveloper@outlook.com>
2025-01-04 19:02:03 -0500

    Update LIBRE_TRANSLATE_URL to use the secure HTTPS endpoint

--- services/translate.js
+++ services/translate.js
@@ -1,4 +1,4 @@
-const LIBRE_TRANSLATE_URL = "http://127.0.0.1:5000/translate";
+const LIBRE_TRANSLATE_URL = "https://libretranslate.com/translate";
 
 export const translateText = async (text) => {
   try {
